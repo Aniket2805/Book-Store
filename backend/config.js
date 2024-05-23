b525b034cce8f34e5da5c44fa40f2eb621e7bd9d
Aniket2805 <aniketrocks2468@gmail.com>
2024-05-24 00:44:08 +0530

    w

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 3000;
 export const URI =
-  "mongodb+srv://aniket:12345@mongointro.edccty0.mongodb.net/?retryWrites=true&w=majority&appName=MongoIntro";
+  "mongodb+srv://aniket:12345@mongointro.edccty0.mongodb.net/test?retryWrites=true&w=majority&appName=MongoIntro";

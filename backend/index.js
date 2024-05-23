7b730e7445ca9feda278fc0a655767ce1319eec0
Aniket2805 <aniketrocks2468@gmail.com>
2024-05-23 23:49:20 +0530

    w

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 // app.use(cors());
 app.use(
   cors({
-    origin: "https://book-store-frontend-rho-self.vercel.app/",
+    origin: ["https://book-store-frontend-rho-self.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })

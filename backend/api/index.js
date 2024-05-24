406c698f8877b53150836685c4b79c0df38265ed
Aniket2805 <aniketrocks2468@gmail.com>
2024-05-24 12:02:39 +0530

    w

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,20 +1,20 @@
 import express from "express";
-import { PORT, URI } from "./config.js";
+import { PORT, URI } from "../config.js";
 import mongoose from "mongoose";
-import booksRoute from "./routes/booksRoute.js";
+import booksRoute from "../routes/booksRoute.js";
 import cors from "cors";
 
 const app = express();
 app.use(express.json());
-// app.use(cors());
-app.use(
-  cors({
-    origin: ["http://localhost:5173"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-    credentials: true,
-  })
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: ["http://localhost:5173"],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//     credentials: true,
+//   })
+// );
 
 app.get("/", (req, res) => {
   return res.json("Hello World!");

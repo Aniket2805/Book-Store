b4a8928ec0caf6dbede9174f1a2794fec053b718
Aniket2805 <aniketrocks2468@gmail.com>
2024-07-09 13:46:04 +0530

    w

--- bookStore/src/App.jsx
+++ bookStore/src/App.jsx
@@ -26,17 +26,17 @@ const App = () => {
         </>
       )}
       <Route path="/books/details/:id" element={<ShowBook />} />
-      {/* {isLoggedIn ? (
-        <> */}
-      <Route path="/user/savedbooks" element={<SavedBooks />} />
-      <Route path="/logout" element={<Logout />} />
-      {/* </>
+      {!isLoggedIn ? (
+        <>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+        </>
       ) : (
-        <> */}
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/signin" element={<SignIn />} />
-      {/* </>
-      )} */}
+        <>
+          <Route path="/user/savedbooks" element={<SavedBooks />} />
+          <Route path="/logout" element={<Logout />} />
+        </>
+      )}
       <Route path="*" element={<Error />} />
     </Routes>
   );
